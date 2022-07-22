d5e7a96366e03a1eb3fc1d192ffa03bb0afa924d
karimhbadr <93216666+karimhbadr1@users.noreply.github.com>
2022-07-22 18:08:59 +0100

    More progress, almost done with Chapter 1, will try to finish it this weekend

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,13 @@
 # pcost.py
 #
 # Exercise 1.27
+def portfolio_cost(filename):
+    with open(filename,'rt') as file:
+        header=next(file)
+        total_cost=0.0
+        for line in file:
+            row=line.split(',')
+            total_cost=total_cost+(float(row[1])*float(row[2]))
+    return(total_cost)
+cost=portfolio_cost('Data/portfolio.csv')
+print('Total cost:',cost)
