2bfc39c21cf01ad3ce1ceb1723b392b88c0339e0
karimhbadr <93216666+karimhbadr1@users.noreply.github.com>
2022-07-25 13:17:34 +0100

    Finished chapter 1

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,13 +1,26 @@
 # pcost.py
 #
 # Exercise 1.27
+import csv
+from fileinput import filename
+import sys
 def portfolio_cost(filename):
     with open(filename,'rt') as file:
-        header=next(file)
+        rows=csv.reader(file)
+        headers=next(file)
         total_cost=0.0
-        for line in file:
-            row=line.split(',')
-            total_cost=total_cost+(float(row[1])*float(row[2]))
+        for row in rows:
+            try:
+                nshares=int(row[1])
+                price=float(row[2])
+                total_cost+=nshares*price
+            except ValueError:
+                print('Bad row:',row)
     return(total_cost)
-cost=portfolio_cost('Data/portfolio.csv')
+
+if len(sys.argv)==2:
+    filename=sys.argv[1]
+else:
+    filename=input('Enter a filename:')
+cost=portfolio_cost(filename)
 print('Total cost:',cost)
